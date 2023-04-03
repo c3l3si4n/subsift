cf97cc0cfc310aa8c6d2482e9d50c4888cd83c16
Celesian <insidetf2@gmail.com>
2023-04-03 07:24:26 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,46 @@
+module subsift
+
+go 1.20
+
+require (
+	github.com/cheggaaa/pb v1.0.29
+	github.com/miekg/dns v1.1.52
+	github.com/projectdiscovery/fastdialer v0.0.24
+	github.com/weppos/publicsuffix-go v0.30.0
+)
+
+require (
+	github.com/Mzack9999/go-http-digest-auth-client v0.6.1-0.20220414142836-eb8883508809 // indirect
+	github.com/akrylysov/pogreb v0.10.1 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/dimchansky/utfbom v1.1.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/mattn/go-runewidth v0.0.4 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.21 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/projectdiscovery/hmap v0.0.2-0.20210917080408-0fd7bd286bfa // indirect
+	github.com/projectdiscovery/iputil v0.0.2 // indirect
+	github.com/projectdiscovery/networkpolicy v0.0.4 // indirect
+	github.com/projectdiscovery/retryabledns v1.0.21 // indirect
+	github.com/projectdiscovery/retryablehttp-go v1.0.10 // indirect
+	github.com/projectdiscovery/stringsutil v0.0.2 // indirect
+	github.com/projectdiscovery/utils v0.0.4-0.20230117135930-7371ae6a739d // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	github.com/ulule/deepcopier v0.0.0-20200430083143-45decc6639b6 // indirect
+	github.com/yl2chen/cidranger v1.0.2 // indirect
+	github.com/zmap/rc2 v0.0.0-20131011165748-24b9757f5521 // indirect
+	github.com/zmap/zcrypto v0.0.0-20220803033029-557f3e4940be // indirect
+	go.etcd.io/bbolt v1.3.6 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/tools v0.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ subsift.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/miekg/dns"
+)
+
+func main() {
+
+	log.SetOutput(os.Stderr)
+
+	subdomains := LoadSubdomainsFromFile(os.Args[1])
+	ParseSubdomains(subdomains)
+}
+
+func GenerateRandLowercaseString() string {
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz")
+	b := make([]rune, 8)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func ResolveDomain(domain string) (bool, error) {
+	c := new(dns.Client)
+	m := new(dns.Msg)
+	m.SetQuestion(dns.Fqdn(domain), dns.TypeA)
+	// set timeout
+	c.Net = "udp"
+	c.DialTimeout = 1000 * 1000
+	m.RecursionDesired = true
+	r, _, err := c.Exchange(m, GetRandomNameserver())
+	if err != nil {
+		log.Printf("DNS query failed: %s\n", err.Error())
+		// check if timeout
+		if err.Error() == "i/o timeout" {
+			return false, err
+		}
+
+		return false, nil
+	}
+	if r.Rcode != dns.RcodeSuccess {
+		log.Printf("Invalid answer name %s after MX query for %s\n", domain, domain)
+		return false, nil
+	}
+	if len(r.Answer) < 1 {
+		log.Printf("No records found for %s\n", domain)
+		return false, nil
+	}
+	return true, nil
+}
+
+func ResolveDomainRetry(domain string, retries int) bool {
+	for i := 0; i < retries; i++ {
+		resolved, err := ResolveDomain(domain)
+		if err != nil {
+			continue
+		}
+		return resolved
+	}
+
+	return false
+}
+
+func TestDomainForWildcards(domain string, wildcardMap map[string]bool, mu *sync.Mutex) bool {
+	subdomainToTest := GenerateRandLowercaseString() + "." + domain
+	if ResolveDomainRetry(subdomainToTest, 3) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func LoadSubdomainsFromFile(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var subdomains []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		subdomains = append(subdomains, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return subdomains
+}
+
+func GetRandomNameserver() string {
+	/*
+			185.228.168.9
+		149.112.112.112
+		1.0.0.1
+		1.1.1.1
+		216.146.35.35
+		208.67.220.220
+		208.67.222.222
+		216.146.36.36
+		64.6.64.6
+		64.6.65.6
+		205.171.3.65
+		195.46.39.40
+		76.76.10.0
+		76.76.2.0
+		8.8.4.4
+		195.46.39.39
+		185.228.169.9
+		134.195.4.2
+		8.20.247.20
+		9.9.9.9
+		205.171.2.65
+		8.26.56.26
+		94.140.14.14
+		77.88.8.1
+		94.140.15.15
+		84.200.70.40
+		76.223.122.150
+		76.76.19.19
+	*/
+	nameservers := []string{
+		"1.1.1.1:53",
+		"1.0.0.1:53",
+		"8.8.8.8:53",
+		"8.8.4.4:53",
+		"9.9.9.9:53",
+	}
+	return nameservers[rand.Intn(len(nameservers))]
+
+}
+
+func ParseSubdomains(subdomains []string) []string {
+	var parsedSubdomains []string
+	var mu sync.Mutex
+	// hashmap to track subdomain zones that have already been seen, true if wildcard detected
+	wildcardMap := make(map[string]bool)
+	wg := &sync.WaitGroup{}
+	subdomainChan := make(chan string)
+	// worker pool
+	numWorkers := 110
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for subdomain := range subdomainChan {
+				zone := subdomain[strings.Index(subdomain, ".")+1:]
+
+				mu.Lock()
+				val, ok := wildcardMap[zone]
+				mu.Unlock()
+
+				if ok && val {
+					continue
+				}
+
+				if !ok {
+					isWildcardZone := TestDomainForWildcards(zone, wildcardMap, &mu)
+					mu.Lock()
+					wildcardMap[zone] = isWildcardZone
+					mu.Unlock()
+
+					if isWildcardZone {
+						continue
+					}
+				}
+
+				isWildcardSubdomain := TestDomainForWildcards(subdomain, wildcardMap, &mu)
+				if !isWildcardSubdomain {
+					mu.Lock()
+					parsedSubdomains = append(parsedSubdomains, subdomain)
+					mu.Unlock()
+				}
+			}
+		}()
+	}
+
+	// feed the worker pool
+	for _, subdomain := range subdomains {
+		subdomainChan <- subdomain
+	}
+
+	close(subdomainChan)
+	wg.Wait()
+
+	for _, subdomain := range parsedSubdomains {
+		fmt.Println(subdomain)
+	}
+	return parsedSubdomains
+}
